7760f2fe92e6e41a0843a8c0bd9fa1875cb672ef
DenisPerevoznik <den.perevoznik@gmail.com>
2021-04-21 00:54:09 +0300

    implementing create product page

--- client-a/src/app/pages/products-page/create-product-page/create-product-page.component.ts
+++ client-a/src/app/pages/products-page/create-product-page/create-product-page.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Category } from '@app/shared/interfaces';
+import { CompanyService } from '@app/shared/services/company.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+
+interface IngredientRowModel {
+  ingredientId: number;
+  specificConsumption: number;
+  unitPrice: number;
+  costPrice: number;
+}
 
 @Component({
   selector: 'app-create-product-page',
@@ -11,7 +20,11 @@ import { takeUntil } from 'rxjs/operators';
 export class CreateProductPageComponent implements OnInit, OnDestroy {
 
   unsubscribe: Subject<void> = new Subject();
-  constructor(private route: ActivatedRoute) { }
+  pageTitle = '';
+  isPurchased = false;
+  categories: Category[] = [];
+  ingredientsRows: IngredientRowModel[] = [];
+  constructor(private route: ActivatedRoute, private company: CompanyService) { }
 
   ngOnDestroy(): void {
     this.unsubscribe.next();
@@ -19,10 +32,28 @@ export class CreateProductPageComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+
+    // this.ingredientsRows = [
+    //   {costPrice: 50, ingredientId: 1, specificConsumption: 1, unitPrice: 10},
+    //   {costPrice: 50, ingredientId: 1, specificConsumption: 1, unitPrice: 10},
+    //   {costPrice: 50, ingredientId: 1, specificConsumption: 1, unitPrice: 10},
+    //   {costPrice: 50, ingredientId: 1, specificConsumption: 1, unitPrice: 10}
+    // ]
+    this.getCategories();
+
+    this.route.queryParams
+    .pipe(takeUntil(this.unsubscribe))
+    .subscribe(params => {
+
+      if('isPurchased' in params){
+        this.isPurchased = params.isPurchased == 'true' ? true : false;
+      }
+      this.pageTitle = this.isPurchased ? 'Добавление товара' : 'Создание тех карты';
+    })
+
     this.route.params
     .pipe(takeUntil(this.unsubscribe))
     .subscribe(params => {
-      
       if(params.id){ // isEdit
 
       }
@@ -32,4 +63,9 @@ export class CreateProductPageComponent implements OnInit, OnDestroy {
     });
   }
 
+  getCategories(){
+    this.company.getCategories()
+    .pipe(takeUntil(this.unsubscribe))
+    .subscribe(categories => {this.categories = categories})
+  }
 }
